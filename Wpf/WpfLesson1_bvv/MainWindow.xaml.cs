4876b0b07df0836be62f97468092292d96c388a7
Chevapc7ch <121220380+Chevapc7ch@users.noreply.github.com>
2024-05-11 12:03:01 +0300

    Add files via upload

---
+++ Wpf/WpfLesson1_bvv/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System; // Ошибка доавблена #
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLesson1_bvv // отсусотваовал namespace
+{
+    public partial class MainWindow : Window
+    {
+        private const string LOGIN = "admin"; // Ошибка в написании String должно быть string
+        private const string PASS = "123456"; // Таже самая ошибка
+
+        public MainWindow()
+        {
+            InitializeComponent(); // отсуствовал элимент ;
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginTextBox.Text.Equals(LOGIN) && passPasswordBox.Password.Equals(PASS))
+            {
+                MessageBox.Show("Authorization successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Authorization failed!", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void loginTextBox_TextChanged(object sender, TextChangedEventArgs e) // Добавлен Текст Бокс
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // Добавлена кнопка выхода
+        {
+            Close(); // Закрытие окна
+        }
+    }
+}
